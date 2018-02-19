d24b8609b069209c08ba471afe76f8436d2b2321
Gabriel Rodriguez - Torres <gabrielrguez0518@gmail.com>
2018-02-19 18:21:43 -0500

    Time possesion started.

--- app/src/main/java/com/garoto/musscout/DisplayRobotScout.java
+++ app/src/main/java/com/garoto/musscout/DisplayRobotScout.java
@@ -1,13 +1,39 @@
 package com.garoto.musscout;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 //import android.widget.Button;
 //import android.widget.EditText;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DisplayRobotScout extends AppCompatActivity {
+
+    Button btnStart, btnPause, btnLap;
+    TextView txtTimer;
+    Handler customHandler = new Handler();
+    LinearLayout container;
+
+    long startTime = 0L, timeInMillisseconds = 0L, timeSwapBuff = 0L, updateTime=0L;
+    Runnable updateTimeThread = new Runnable() {
+        @Override
+        public void run() {
+            timeInMillisseconds = SystemClock.uptimeMillis()-startTime;
+            updateTime = timeSwapBuff + timeInMillisseconds;
+            int secs = (int)( updateTime/1000);
+            int mins = secs/60;
+            secs%=60;
+            int milliseconds = (int) (updateTime%1000);
+            txtTimer.setText(""+mins+":"+String.format("%2d",secs) +":"+String.format("%3d",milliseconds));
+            customHandler.postDelayed(this,0);
+        }
+    };
 
     private int allianceCounter = 0;
     private TextView allianceTextView;
@@ -30,6 +56,38 @@ public class DisplayRobotScout extends AppCompatActivity {
         scaleTextView = (TextView) findViewById(R.id.scaleTextView);
         opponentTextView = (TextView) findViewById(R.id.opponentTextView);
         exchangeTextView = (TextView) findViewById(R.id.exchangeTextView);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnPause = (Button) findViewById(R.id.btnPause);
+        btnLap = (Button) findViewById(R.id.btnLap);
+        txtTimer = (TextView) findViewById(R.id.timerValue);
+        container = (LinearLayout) findViewById(R.id.container);
+
+        btnStart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimeThread,0);
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                timeSwapBuff += timeInMillisseconds;
+                customHandler.removeCallbacks(updateTimeThread);
+            }
+        });
+
+        btnLap.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View addView = inflater.inflate(R.layout.row,null);
+                TextView txtValue = (TextView)addView.findViewById(R.id.txtContent);
+                txtValue.setText(txtTimer.getText());
+                container.addView(addView);
+            }
+        });
         //        Button robotSubmit = (Button)findViewById(R.id.robotSubmit);
 //
 //        robotSubmit.setOnClickListener(new View.OnClickListener(){
